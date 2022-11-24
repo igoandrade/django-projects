3f455ab0e44b2a52182f8ba4c641356503672582
Igo da Costa Andrade <igoandrade@uft.edu.br>
2022-11-24 16:33:53 -0300

    Criação do modelos de banco de dados

--- ecommerce-project/store/models.py
+++ ecommerce-project/store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+
